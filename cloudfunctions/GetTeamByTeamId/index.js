d72da9fc933533f9276b30ed5f134642a39fde29
SnowfungusWang <kingsnowmushroom@126.com>
2022-04-18 10:17:20 +0800

    fix：获取teamlist函数

--- cloudfunctions/GetTeamByTeamId/index.js
+++ cloudfunctions/GetTeamByTeamId/index.js
@@ -6,31 +6,27 @@ const db = cloud.database()
 const _ = db.command
 exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
-  let cps;
   try {
-    let teams = await db.collection('user_team').where({
-      userId: wxContext.OPENID
+    let t =  await db.collection('teams').where({
+      _id: event.teamId
     }).get()
-
-    console.log(teams)
-    
-    if (teams.data.length == 0) return { data: [], errMsg: "用户未在任何一个团队中" }
-    let allteams = await db.collection('teams').where({
-      _id: _.in(teams.data[0].teams)
-    }).get()
-
-    if (allteams.data.length !== 0) {
-      for (let i in allteams.data) {
-        for (let j in allteams.data[i].memberList) {
-          let su = await db.collection('users').where({
-            userId: allteams.data[i].memberList[j]
-          }).get()
-          allteams.data[i].memberList[j] = su.data[0]
-        }
+    // let u = new Array()
+    // t.data[0].memberList.map(async (user, index, arr) => {
+    //   let su = await db.collection('users').where({
+    //     userId: user
+    //   }).get()
+    //   return arr[index] = su.data[0]
+    // })
+    // return t
+    if (t.data.length !== 0) {
+      for (let i in t.data[0].memberList) {
+        let su = await db.collection('users').where({
+          userId: t.data[0].memberList[i]
+        }).get()
+        t.data[0].memberList[i] = su.data[0]
       }
     }
-
-    return allteams
+    return t
   } catch (e) {
     console.error(e)
   }
